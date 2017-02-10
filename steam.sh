90079a8e70b03c5662b89c250e182dfca3073a02
Simone Caronni <negativo17@gmail.com>
2017-02-10 19:08:28 +0100

    - Remove noruntime subpackage, use default new mechanism that uses host libraries as per client update of 19th January (5th January for beta): http://store.steampowered.com/news/26953/ - Add libdbusmenu-gtk3 library requirement on Fedora (luckily not RHEL/CentOS). - Remove patch for window button behaviour, use shell profile.

--- steam.sh
+++ steam.sh
@@ -1,2 +1,9 @@
-# Disable Ubuntu 32 bit runtime
-[ -x /usr/bin/steam ] && export STEAM_RUNTIME=0
+# Improved interactions between the Steam runtime and host distribution
+# libraries, which should let Steam work out of the box with open-source
+# graphics drivers on modern distributions. If using an older distribution or
+# running into problems, set variable at 0 to revert to the previous behavior.
+# STEAM_RUNTIME_PREFER_HOST_LIBRARIES=0
+
+# Unify close-to-tray behavior with other platforms. If using a distribution
+# that doesn't have proper compatible tray support set variable at 0.
+STEAM_FRAME_FORCE_CLOSE=1
